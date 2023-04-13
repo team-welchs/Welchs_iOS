feb5141f4a98ffb03cdf3e20da86e20746d55f99
유현명 <aahspringaa4@naver.com>
2023-04-13 14:18:00 +0900

    Add :: Init Project

---
+++ Welchs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Welchs
+//
+//  Created by 유현명 on 2023/04/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WelchsUITests/WelchsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WelchsUITestsLaunchTests.swift
+//  WelchsUITests
+//
+//  Created by 유현명 on 2023/04/13.
+//
+
+import XCTest
+
+final class WelchsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
